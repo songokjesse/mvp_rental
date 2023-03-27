e47635a0941d95cdf5d7dd135e4a7ad36025f25b
Jesse Songok <jessesongok@gmail.com>
2023-03-27 12:56:28 +0300

    House Image Upload Livewire

--- resources/views/livewire/house-upload-image.blade.php
+++ resources/views/livewire/house-upload-image.blade.php
@@ -5,7 +5,7 @@
             Image Preview:
                 <img src="{{ $photo->temporaryUrl() }}" class="mt-3 mb-3">
         @endif
-        <input type="file" wire:model="photo" class="form-control">
+        <input type="file" wire:model="photo" class="form-control"" >
         @error('photo') <span class="error">{{ $message }}</span> @enderror
         <button type="submit" class="btn btn-primary btn-sm mt-3"><i class="bi bi-save"></i> Save Images</button>
 
