d01fe621b44380c3d6b1c2d6051e32d2b496d672
Jesse Songok <jessesongok@gmail.com>
2023-04-11 12:40:26 +0300

    HomePage  Refactor

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -74,6 +74,17 @@
         <main class="py-4">
             @yield('content')
         </main>
+        <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top container">
+            <div class="col-md-4 d-flex align-items-center">
+
+                <span class="mb-3 mb-md-0 text-muted">© 2023 Codelab</span>
+            </div>
+
+            <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+                <li class="ms-3"><a class="text-muted" href="#"><i class="bi bi-instagram"></i></a></li>
+                <li class="ms-3"><a class="text-muted" href="#"></a><i class="bi bi-twitter"></i></li>
+            </ul>
+        </footer>
     </div>
     @livewireScripts
 {{--    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>--}}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -29,6 +29,17 @@
             @livewire('property-filter')
         </div>
     </main>
+    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top container">
+        <div class="col-md-4 d-flex align-items-center">
+
+            <span class="mb-3 mb-md-0 text-muted">© 2023 Codelab</span>
+        </div>
+
+        <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+            <li class="ms-3"><a class="text-muted" href="#"><i class="bi bi-instagram"></i></a></li>
+            <li class="ms-3"><a class="text-muted" href="#"></a><i class="bi bi-twitter"></i></li>
+        </ul>
+    </footer>
 </div>
 
 @livewireScripts
