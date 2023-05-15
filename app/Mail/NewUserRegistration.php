7ece35c250fdeb7a36b485c29420762c03c56f05
Jesse Songok <jessesongok@gmail.com>
2023-05-15 12:39:49 +0300

    Update NewUserRegistration.php

--- app/Mail/NewUserRegistration.php
+++ app/Mail/NewUserRegistration.php
@@ -39,7 +39,7 @@ class NewUserRegistration extends Mailable
     public function content(): Content
     {
         return new Content(
-            view: 'view.name',
+            view: 'emails.new-user-registration',
         );
     }
 
