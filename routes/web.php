5d19d21f026eb1e494b94d4d0f7909411326b835
Jesse Songok <jessesongok@gmail.com>
2023-05-16 18:42:06 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Route::get('/', function () {
     return view('welcome', compact('properties'));
 });
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
